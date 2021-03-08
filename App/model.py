ac76eb4db79ac5d2954faef57775dde8528d5213
Pablo Ortega <p.ortegac@uniandes.edu.co>
2021-03-08 16:24:39 -0500

    Requisito 4 (3)

--- App/model.py
+++ App/model.py
@@ -109,10 +109,13 @@ def videos_pais_categoria(catalog,pais,nombre_categoria,n):
 def videos_pais_tag(catalog,pais2,tag,cantidad):
     i=1
     titles=[]
-    sub_list=lt.newList('ARRAY_LIST')
-    while i <  (lt.size(catalog['videos'])):
+    sub_list= mrg.sort(catalog['videos'],cmpVideosbyViews)
+    sub_list = sub_list.copy()
+
+    subsub_list=lt.newList('ARRAY_LIST')
+    while i <  (lt.size(sub_list)):
         
-        str_tags= catalog['videos']['elements'][i]['tags']
+        str_tags= sub_list['elements'][i]['tags']
         str_tags_clean1= str_tags.replace('"','')
         str_tags_clean1= str_tags_clean1.replace('(','')
         str_tags_clean1= str_tags_clean1.replace(')','')
@@ -121,18 +124,16 @@ def videos_pais_tag(catalog,pais2,tag,cantidad):
         list_tags1=str_tags_clean1.split('|')
         list_tags2=str_tags_clean2.split()
         list_tags3 = list_tags1 + list_tags2
-        if  (pais2 in ((catalog['videos']['elements'][i]['country']).lower())) and (tag in list_tags3) and ((catalog['videos']['elements'][i]['title']) not in titles):
-            lt.addLast(sub_list, catalog['videos']['elements'][i])
-            titles.append(catalog['videos']['elements'][i]['title'])
+        if  (pais2 in ((sub_list['elements'][i]['country']).lower())) and (tag in list_tags3) and ((sub_list['elements'][i]['title']) not in titles):
+            lt.addLast(subsub_list, sub_list['elements'][i])
+            titles.append(sub_list['elements'][i]['title'])
         
         i+=1
 
-    sub_list = sub_list.copy()
-    mrg.sort(sub_list,cmpVideosbyLikes)
-    subsub_list = lt.subList(sub_list, 1, cantidad)
+    subsub_list = lt.subList(subsub_list, 1, cantidad)
     subsub_list = subsub_list.copy()
 
-
+   
 
     return subsub_list
 
